22dad40008b872a83372ca9f2d4923319ce7e588
Aaron Hung <ahx5x16+github@gmail.com>
2025-12-06 14:11:19 -0500

    Restructured repo to be frontend-only

---
+++ README.md
@@ -0,0 +1,3 @@
+## CS2200 Online
+
+This is just my own project building a website for web resources for the Georgia Tech CS2200 course.
--- backend/src/api/health/health.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getHealthy } from "./health.handlers";
-
-const router = Router();
-
-router.get("/", getHealthy);
-
-export default router;
